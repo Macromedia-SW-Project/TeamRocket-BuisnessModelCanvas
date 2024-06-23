d556f0d355a821861733b4818afa88f54ef937aa
ninja666gaming <ninja666gaming@gmail.com>
2024-06-23 05:23:05 -0600

    Update SetVariables.cs

--- Assets/SetVariables.cs
+++ Assets/SetVariables.cs
@@ -5,10 +5,9 @@ using UnityEngine.AI;
 
 public class SetVariables : MonoBehaviour
 {
+    public GameObject CopiedEnemy;
     static public float SpeedReference = 2;
     static public int HealthReference = 5;
-    public int health;
-    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +17,29 @@ public class SetVariables : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SpeedReference = speed; HealthReference = health;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(CopiedEnemy, transform.position, Quaternion.identity);
+        }
     }
 
-    public void NewEnemy()
+    public void SpawnEnemy()
     {
-        
+        Instantiate(CopiedEnemy, transform.position, Quaternion.identity);
+    }
+    public void Speedy()
+    {
+        SpeedReference = 4;
+        HealthReference = 7;
+    }
+    public void Normal()
+    {
+        SpeedReference = 2;
+        HealthReference = 10;
+    }
+    public void Boss()
+    {
+        SpeedReference = 1;
+        HealthReference = 100;
     }
 }
